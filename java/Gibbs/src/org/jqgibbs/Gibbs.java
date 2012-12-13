d7528c7afef7ab92fade88b192d7b25ce4873c4a
Ewan Dunbar <ewan.dunbar@utoronto.ca>
2012-12-13 14:36:52 -0500

    created 0/500 dump.out

--- java/Gibbs/src/org/jqgibbs/Gibbs.java
+++ java/Gibbs/src/org/jqgibbs/Gibbs.java
@@ -42,7 +42,7 @@ public class Gibbs {
 	private static String dataFileName = "/Users/ewan/Work/School/Projects/dlp/java/Gibbs/spanish_mfiau_f0.txt";
 	//private static String dataFileName = "/Users/jsf/Desktop/eclipse/gibbs/dlp/java/Gibbs/spanish_mfiau_f0.txt";
 	private static int maxIter = 500;//1000;
-	private static int burnIn = 10;//500;
+	private static int burnIn = 0;//500;
 	private static int lag = 5;
 	private static String outFileName = "dump.out";
 
